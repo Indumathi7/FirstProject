d946513f54ec6af7a7ad2534d15607164300454d
Indumathi7 <93659980+Indumathi7@users.noreply.github.com>
2024-06-07 02:23:17 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,6 +1,6 @@
 public class Sample{
   public static void main(String[] args){
-    System.out.println("Helloo World");
+    System.out.println("Helloo World Sub");
     System.out.println("Helloo World!");
   }
 }
